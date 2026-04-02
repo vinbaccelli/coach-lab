145cf19b6834b06894fede07d28234a5730104cb
vinbaccelli <vinbaccelli@gmail.com>
2026-04-02 15:53:32 +0200

    Remove webpack fallback for canvas in next.config.js
    Removed custom webpack fallback configuration for canvas.

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      canvas: require.resolve('canvas'),
-      ...config.resolve.fallback,
-    };
-    return config;
-  },
   swcMinify: true,
 };
 
